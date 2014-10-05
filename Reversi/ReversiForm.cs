9e7b4e8c8ec5bd8a1d227f6d41d6b7d296feffd5
gmlent <goosvanlent@gmail.com>
2014-10-05 17:00:08 +0200

    Commentaar toegevoegd

--- Reversi/ReversiForm.cs
+++ Reversi/ReversiForm.cs
@@ -12,7 +12,8 @@ namespace Reversi
 {
    public partial class ReversiForm : Form
    {
-
+      Rectangle[,] rect;
+      int[,] Bord;
       int w = 6;                  //breedte bord
       int h = 6;                  //hoogte bord
       int gBlokje = 40;           //breedte en hoogte (grootte) van elk blokje
@@ -21,6 +22,8 @@ namespace Reversi
       public ReversiForm()
       {
          InitializeComponent();
+         Bord = new int[w, h];
+         rect = new Rectangle[w, h];
          panel1.Paint += this.TekenBord;
          panel1.MouseClick += Bord_MouseClick;
          panel1.MouseMove += Bord_MouseMove;
@@ -40,19 +43,61 @@ namespace Reversi
       }
       public void TekenBord(object obj, PaintEventArgs pea)
       {
-         int[,] Bord;
-         Bord = new int[w, h];
+          Graphics g = pea.Graphics;
+          Pen blackPen = new Pen(Brushes.Black, 1);
+          Brush Blue = new SolidBrush(Color.Blue);
+          Brush Red = new SolidBrush(Color.Red);
 
 
-         for (int x = 0; x < w; x++)
-         {
-            for (int y = 0; y < h; y++)
-               pea.Graphics.DrawRectangle(Pens.Black, x * gBlokje, y * gBlokje, gBlokje, gBlokje);
-         }
 
-         pea.Graphics.FillEllipse(Brushes.Red, muisX - (gBlokje / 2), muisY - (gBlokje / 2), gBlokje - 6, gBlokje - 6);
+          for (int x = 0; x < w; x++)
+          {
+              for (int y = 0; y < h; y++)
+              {
+                  Bord[x, y] = -1;
+              }
+          }
+          /*
+          Installeert de beginstenen
+           */
+          Bord[w / 2 - 1, h / 2 - 1] = 0;
+          Bord[w / 2, h / 2] = 0;
+          Bord[w / 2 - 1, h / 2] = 1;
+          Bord[w / 2, h / 2 - 1] = 1;
 
+          /* 
+          Installeert de vakjes
+           */
+          for (int x = 0; x < w; x++)
+          {
+              for (int y = 0; y < h; y++)
+              {
+                  rect[x, y] = new Rectangle(x * (panel1.Width / w), y * (panel1.Height / h), panel1.Width / w, panel1.Height / h);
+              }
+          }
 
+          for (int x = 0; x < w; x++)
+          {
+              for (int y = 0; y < h; y++)
+              {
+                  g.DrawRectangle(blackPen, rect[x, y]);
+              }
+          }
+
+          for (int x = 0; x < w; x++)
+          {
+              for (int y = 0; y < h; y++)
+              {
+                  if (Bord[x, y] == 0)
+                  {
+                      g.FillEllipse(Blue, rect[x, y]);
+                  }
+                  else if (Bord[x, y] == 1)
+                  {
+                      g.FillEllipse(Red, rect[x, y]);
+                  }
+              }
+          }
       }
    }
 }
