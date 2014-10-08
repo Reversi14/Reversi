47f0315771306b2d9830179eb02aeadd0b41e758
Billum <tomas.billum@outlook.com>
2014-10-08 09:41:58 +0200

    Aanpassingen

--- Reversi/ReversiForm.cs
+++ Reversi/ReversiForm.cs
@@ -12,92 +12,92 @@ namespace Reversi
 {
    public partial class ReversiForm : Form
    {
-      Rectangle[,] rect;
-      int[,] Bord;
-      int w = 6;                  //breedte bord
-      int h = 6;                  //hoogte bord
-      int gBlokje = 40;           //breedte en hoogte (grootte) van elk blokje
-      int muisX, muisY;
-
+      Rectangle[,] rect;                           // rechthoekige array
+      Rectangle[,] steen;                          // stenen in de array
+      int[,] Bord;                                 // waardes in de array
+      int w = 8;                                   // aantal vakjes breed
+      int h = 8;                                   // aantal vakjes hoog
+      int X, Y;
       public ReversiForm()
       {
          InitializeComponent();
          Bord = new int[w, h];
          rect = new Rectangle[w, h];
-         panel1.Paint += this.TekenBord;
-         panel1.MouseClick += Bord_MouseClick;
-         panel1.MouseMove += Bord_MouseMove;
+         steen = new Rectangle[w, h];
+
+         this.Paint += ReversiForm_Paint;
+         panel1.Paint += panel1_Paint;
+         panel1.MouseClick += panel1_MouseClick;
+         panel1.MouseMove += panel1_MouseMove;
+      }
+      void ReversiForm_Paint(object sender, PaintEventArgs pea)
+      {
+         // Tekent een rode en blauwe steen bij aantalen stenen voor de spelers.
+         Graphics g = pea.Graphics;
+         g.DrawImage(Reversi.Properties.Resources.img_blue, 65, 68, 15, 15);
+         g.DrawImage(Reversi.Properties.Resources.img_red, 65, 95, 15, 15);
       }
 
-      void Bord_MouseMove(object sender, MouseEventArgs e)
+      void panel1_MouseMove(object sender, MouseEventArgs e)
       {
-
-      }
-
-      public void Bord_MouseClick(object sender, MouseEventArgs e)
-      {
-         muisX = e.X;
-         muisY = e.Y;
-         panel1.Invalidate();
+         this.Invalidate();
          DoubleBuffered = true;
       }
-      public void TekenBord(object obj, PaintEventArgs pea)
+
+      public void panel1_MouseClick(object sender, MouseEventArgs e)
       {
-          Graphics g = pea.Graphics;
-          Pen blackPen = new Pen(Brushes.Black, 1);
-          Brush Blue = new SolidBrush(Color.Blue);
-          Brush Red = new SolidBrush(Color.Red);
+         X = e.X;
+         Y = e.Y;
 
+         panel1.Refresh();
+      }
 
+      public void panel1_Paint(object obj, PaintEventArgs pea)
+      {
+         int breedteVakje = panel1.Width / w;
+         int hoogteVakje = panel1.Height / h;
 
-          for (int x = 0; x < w; x++)
-          {
-              for (int y = 0; y < h; y++)
-              {
-                  Bord[x, y] = -1;
-              }
-          }
-          /*
-          Installeert de beginstenen
-           */
-          Bord[w / 2 - 1, h / 2 - 1] = 0;
-          Bord[w / 2, h / 2] = 0;
-          Bord[w / 2 - 1, h / 2] = 1;
-          Bord[w / 2, h / 2 - 1] = 1;
+         Graphics g = pea.Graphics;
+         Pen blackPen = new Pen(Brushes.Black, 1);  
 
-          /* 
-          Installeert de vakjes
-           */
-          for (int x = 0; x < w; x++)
-          {
-              for (int y = 0; y < h; y++)
-              {
-                  rect[x, y] = new Rectangle(x * (panel1.Width / w), y * (panel1.Height / h), panel1.Width / w, panel1.Height / h);
-              }
-          }
+         for (int x = 0; x < w; x++)
+         {
+            for (int y = 0; y < h; y++)
+            {
+               // Vormgeving bord + alle arrays krijgen waarde -1
+               rect[x, y] = new Rectangle(x * breedteVakje, y * hoogteVakje, breedteVakje, hoogteVakje);
+               g.DrawRectangle(blackPen, rect[x, y]);
 
-          for (int x = 0; x < w; x++)
-          {
-              for (int y = 0; y < h; y++)
-              {
-                  g.DrawRectangle(blackPen, rect[x, y]);
-              }
-          }
+               Bord[x, y] = -1;
 
-          for (int x = 0; x < w; x++)
-          {
-              for (int y = 0; y < h; y++)
-              {
-                  if (Bord[x, y] == 0)
-                  {
-                      g.FillEllipse(Blue, rect[x, y]);
-                  }
-                  else if (Bord[x, y] == 1)
-                  {
-                      g.FillEllipse(Red, rect[x, y]);
-                  }
-              }
-          }
+               // Vormgeving van stenen.
+               // Optel en aftrek handelingen zorgen ervoor dat de steen kleiner dan de rechthoek is
+               // en in het midden van de rechthoek.
+               steen[x, y] = new Rectangle(x * breedteVakje + 2, y * hoogteVakje + 2, breedteVakje - 3, hoogteVakje - 3);
+            }
+         }
+         int a = w / 2;    //midden bord horizontaal.
+         int b = h / 2;    //midden bord verticaal.
+
+         // Beginstand bord d.m.v. waardebepaling.
+         Bord[a - 1, b - 1] = 0;
+         Bord[a, b] = 0;
+
+         Bord[a - 1, b] = 1;
+         Bord[a, b - 1] = 1;
+
+         // Deze waardebepaling hierboven kleur steen toewijzen.
+         for (int x = 0; x < w; x++)
+         {
+            for (int y = 0; y < h; y++)
+            {
+               if (Bord[x, y] == 0) // blauwe stenen
+                  g.DrawImage(Reversi.Properties.Resources.img_blue, steen[x, y]);
+
+               else if (Bord[x, y] == 1) // rode stenen
+                  g.DrawImage(Reversi.Properties.Resources.img_red, steen[x, y]);               
+            }
+         }
       }
    }
 }
