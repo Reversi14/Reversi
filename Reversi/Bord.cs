68c5f0cc5c13624f0f4ed2460d8b4df144e0fb56
Billum <tomas.billum@outlook.com>
2014-10-11 13:22:40 +0200

    New

--- Reversi/Bord.cs
+++ Reversi/Bord.cs
@@ -9,24 +9,27 @@ namespace Reversi
 {
     class Bord
     {
-        public int[,] Stenen;
-        public int w;
-        public int h;
-        public bool beurt;
-        public const int LEEG = -1;
-        public const int BLAUW = 0;
-        public const int ROOD = 1;
+        public int[,] Stenen;                           // Int-array voor de stenen.
+        public int w;                                   // Aantal vakjes breed.
+        public int h;                                   // Aantal vakjes hoog.
+        public bool beurt;                              // Bool om te kijken wie er aan de beurt is.
+        public const int Leeg = -1;                     // Als int-array is '-1' lege rechthoek
+        public const int Player1 = 0;                   // Als '0' is de rechthoek van blauw en wordt er een blauwe steen ingezet.
+        public const int Player2 = 1;                   // Ditzelfde geldt voor rood bij de waarde '1'.
 
         public Bord(int w, int h)
         {
             this.w = w;
             this.h = h;
+
+            // Grootte int-array stenen in 'w' en 'h'.
             Stenen = new int[w, h];
 
-            NieuwSpel();            
+            // Initialiseren van methode nieuwSpel
+            nieuwSpel();
         }
-        
-        public void NieuwSpel()
+
+        public void nieuwSpel()
         {
             // Declaratie hier zorgt ervoor dat blauw altijd begint.
             beurt = true;
@@ -36,7 +39,7 @@ namespace Reversi
             {
                 for (int j = 0; j < h; j++)
                 {
-                    Stenen[i, j] = LEEG;
+                    Stenen[i, j] = Leeg;
                 }
             }
             // Midden bord horizontaal en verticaal.
@@ -44,63 +47,80 @@ namespace Reversi
             int b = h / 2;
 
             // Beginstand bord d.m.v. waardebepaling.
-            Stenen[a, b] = BLAUW;
-            Stenen[a - 1, b - 1] = BLAUW;
+            Stenen[a, b] = Player1;
+            Stenen[a - 1, b - 1] = Player1;
 
-            Stenen[a - 1, b] = ROOD;
-            Stenen[a, b - 1] = ROOD;
+            Stenen[a - 1, b] = Player2;
+            Stenen[a, b - 1] = Player2;
         }
 
-        #region Definities vakjes waarde (isLeeg, -Blauw en -Rood)
+        #region Definities vakjes waarde (isLeeg, -Player1 en -Player2)
+
+        // Makkelijke afkortingen voor veelgebruikte bools.
         public bool isLeeg(int x, int y)
         {
-            return Stenen[x, y] == LEEG;
+            return Stenen[x, y] == Leeg;
         }
-        public bool isBlauw(int x, int y)
+        public bool isPlayer1(int x, int y)
         {
-            return Stenen[x, y] == BLAUW;
+            return Stenen[x, y] == Player1;
         }
-        public bool isRood(int x, int y)
+        public bool isPlayer2(int x, int y)
         {
-            return Stenen[x, y] == ROOD;
+            return Stenen[x, y] == Player2;
         }
         #endregion
 
+        // Geeft einde van het spel aan als het bord vol is of
+        // er geen mogelijke zet is voor de huidige speler.
         public bool eindSpel()
         {
-            return Score(Bord.BLAUW) + Score(Bord.ROOD) == w * h;
+            for (int p = 0; p < w; p++)
+                for (int q = 0; q < h; q++)
+                {
+                    if (geldigeZet(p, q))
+                        return false;
+                }
+            return true;
+
+            return Score(Bord.Player1) + Score(Bord.Player2) == w * h;
         }
 
-
+        // Deze methode geeft aan of je daar een steen mag neerleggen,
+        // wisselt kleur van de stenen die ertussen liggen en
+        // wisselt de beurt.
         public void Klik(int x, int y)
         {
-            if (isLeeg(x, y) && geldigeZet(x,y))
+            if (isLeeg(x, y) && geldigeZet(x, y))
             {
                 if (beurt)
-                    Stenen[x, y] = BLAUW;
+                    Stenen[x, y] = Player1;
                 else
-                    Stenen[x, y] = ROOD;
+                    Stenen[x, y] = Player2;
 
                 omslagKleur(x, y);
                 wisselBeurt();
-                
+
             }
         }
-        public void wisselBeurt()
+
+        // Beurtwissel.
+        private void wisselBeurt()
         {
             beurt = !beurt;
         }
-        
-        public void omslagKleur(int x, int y)
+
+        // Zorgt voor de omslag van kleur van tussenliggende stenen.
+        private void omslagKleur(int x, int y)
         {
             int spelerBeurt;
             int dx;
             int dy;
 
             if (beurt)
-                spelerBeurt = BLAUW;
+                spelerBeurt = Player1;
             else
-                spelerBeurt = ROOD;
+                spelerBeurt = Player2;
 
             for (dx = -1; dx <= 1; dx++)
             {
@@ -116,7 +136,7 @@ namespace Reversi
                                 break;
                             if (Stenen[i, j] == spelerBeurt)
                             {
-                                if ((i > x + 1 && dx == 1) || (i < x - 1 && dx == -1) || (j > y + 1  && dy == 1) || (j < y - 1 && dy == -1))
+                                if ((i > x + 1 && dx == 1) || (i < x - 1 && dx == -1) || (j > y + 1 && dy == 1) || (j < y - 1 && dy == -1))
                                 {
                                     wisselKleur(x, y, i, j, dx, dy);
                                     break;
@@ -135,6 +155,7 @@ namespace Reversi
             }
         }
 
+        // Wisselt de kleur van tussenliggende stenen.
         private void wisselKleur(int x1, int y1, int x2, int y2, int dx, int dy)
         {
             int spelerBeurt;
@@ -142,9 +163,9 @@ namespace Reversi
             int q = y1;
 
             if (beurt)
-                spelerBeurt = BLAUW;
+                spelerBeurt = Player1;
             else
-                spelerBeurt = ROOD;
+                spelerBeurt = Player2;
 
             while ((p > x2 && dx == -1) || (p < x2 && dx == 1) || (q > y2 && dy == -1) || (q < y2 && dy == 1))
             {
@@ -155,6 +176,7 @@ namespace Reversi
             }
         }
 
+        // Checkt of er een zet mogelijk is.
         public bool geldigeZet(int x, int y)
         {
             int spelerBeurt;
@@ -162,19 +184,19 @@ namespace Reversi
             int dy;
 
             if (beurt)
-                spelerBeurt = BLAUW;
+                spelerBeurt = Player1;
             else
-                spelerBeurt = ROOD;
+                spelerBeurt = Player2;
 
-            for ( dx = -1; dx <= 1; dx++)
+            for (dx = -1; dx <= 1; dx++)
             {
-                for ( dy = -1; dy <= 1; dy++)
+                for (dy = -1; dy <= 1; dy++)
                 {
-                    if( !(dx == 0 && dy == 0))
+                    if (!(dx == 0 && dy == 0))
                     {
                         int i = x + dx;
                         int j = y + dy;
-                        while( i >= 0 && j >= 0 && i < w && j < h)
+                        while (i >= 0 && j >= 0 && i < w && j < h)
                         {
                             if (isLeeg(i, j))
                                 break;
@@ -189,7 +211,7 @@ namespace Reversi
                             {
                                 i += dx;
                                 j += dy;
-                            }                            
+                            }
                         }
                     }
                 }
@@ -197,6 +219,7 @@ namespace Reversi
             return false;
         }
 
+        // Geeft aantal stenen van de spelers.
         public int Score(int Speler)
         {
             int resultaat = 0;
@@ -212,10 +235,24 @@ namespace Reversi
             }
             return resultaat;
         }
+
+        // Geeft aan wie er aan de beurt is.
         public string Spelers()
         {
             string resultaat = "";
+
             if (beurt)
+                return resultaat += "Blauw";
+            else
+                return resultaat += "Rood";
+        }
+
+        // Laat zien wie er een hogere score heeft.
+        public string hoogsteScore()
+        {
+            string resultaat = "";
+
+            if (Score(Player1) > Score(Player2))
                 return resultaat += "Blauw";
             else
                 return resultaat += "Rood";
--- Reversi/ReversiForm.cs
+++ Reversi/ReversiForm.cs
@@ -12,64 +12,74 @@ namespace Reversi
 {
     public partial class ReversiForm : Form
     {
-        Rectangle[,] steen;
-        Rectangle[,] rect;
-        Bord bord;
-        int X, Y;
-        bool hulp;
-        
+        Rectangle[,] steen;                         // Array voor de stenen.
+        Rectangle[,] rect;                          // Array voor de rechthoeken.
+        Bord bord;                                  // Verwijzing naar Bord klasse
+        int X, Y;                                   // Muis X en Y declaratie
+        bool hulp;                                  // Bool voor hulpknop.
+
         public ReversiForm()
         {
             InitializeComponent();
-            hulp = false;
-            bord = new Bord(6, 6);
-            rect = new Rectangle[bord.w, bord.h];
-            steen = new Rectangle[bord.w, bord.h];
+            hulp = false;                           // Hulpknop staat door 'false'-waarde uit in het begin.
+            bord = new Bord(8, 8);                 // Hoogte en breedte instellen van bord.
+            rect = new Rectangle[bord.w, bord.h];   // aantal rechthoeken in breedte en hoogte, resp. 'w' en 'h'.
+            steen = new Rectangle[bord.w, bord.h];  // aantal stenen                  "
 
+            // Methoden voor het Form
             this.Paint += ReversiForm_Paint;
             panel1.Paint += panel1_Paint;
-            panel1.MouseClick += panel1_MouseClick;            
+            panel1.MouseClick += panel1_MouseClick;
         }
 
         void ReversiForm_Paint(object sender, PaintEventArgs pea)
         {
-            // Tekent een rode en blauwe steen op het form.
+            // Tekent een rode en blauwe steen op het Form.
             pea.Graphics.DrawImage(Reversi.Properties.Resources.img_blue, 105, 68, 15, 15);
             pea.Graphics.DrawImage(Reversi.Properties.Resources.img_red, 105, 95, 15, 15);
         }
 
         public void panel1_MouseClick(object sender, MouseEventArgs e)
         {
+            // Muislocatie
             X = e.X;
             Y = e.Y;
 
+            // Bepaling in welke rechthoek je zit met je muis.
             int i, j;
             i = X / (panel1.Width / bord.w);
             j = Y / (panel1.Height / bord.h);
 
+            // Initialiseren van methode Klik in klasse Bord.
             bord.Klik(i, j);
-           
+
+            // Toepassen op panel.
             panel1.Invalidate();
             DoubleBuffered = true;
         }
 
         public void panel1_Paint(object obj, PaintEventArgs pea)
         {
+            // Declaratie grootte vakjes, de '-1' zorgt ervoor dat er beneden en links 
+            // niet buiten het panel wordt getekend.
             int breedteVakje = (panel1.Width - 1) / bord.w;
             int hoogteVakje = (panel1.Height - 1) / bord.h;
 
-            label1.Text = string.Format("heeft {0} veld(en)", bord.Score(Bord.BLAUW));
-            label2.Text = string.Format("heeft {0} veld(en)", bord.Score(Bord.ROOD));
+            // Updaten van scores van beide spelers.
+            label1.Text = string.Format("heeft {0} veld(en)", bord.Score(Bord.Player1));
+            label2.Text = string.Format("heeft {0} veld(en)", bord.Score(Bord.Player2));
 
+            // Tijdens het spel zeggen wie er aan de beurt is en
+            // aan het einde van het spel (vol bord of geen mogelijke zetten) uitslag geven.
             if (bord.eindSpel())
             {
-                if (bord.Score(Bord.BLAUW) > bord.Score(Bord.ROOD))
+                if (bord.Score(Bord.Player1) > bord.Score(Bord.Player2))
                 {
-                    label3.Text = String.Format("Blauw heeft gewonnen!");
+                    label3.Text = String.Format("{0} heeft gewonnen!", bord.hoogsteScore());
                 }
-                else if (bord.Score(Bord.ROOD) > bord.Score(Bord.BLAUW))
+                else if (bord.Score(Bord.Player2) > bord.Score(Bord.Player1))
                 {
-                    label3.Text = String.Format("Rood heeft gewonnen!");
+                    label3.Text = String.Format("{0} heeft gewonnen!", bord.hoogsteScore());
                 }
                 else
                 {
@@ -80,11 +90,12 @@ namespace Reversi
             {
                 label3.Text = String.Format("{0} aan zet...", bord.Spelers());
             }
-            
-           
+
+
             Graphics g = pea.Graphics;
             Pen blackPen = new Pen(Brushes.Black, 1);
-            // afstand en bepaling  voor de steen en hulp rechthoek.
+
+            // Plaats bepalen voor de steen en hulp rechthoek d.m.v. constanten.
             int a = 2;
             int b = 2 * a;
             int c = 2 * b;
@@ -108,12 +119,13 @@ namespace Reversi
             {
                 for (int j = 0; j < bord.h; j++)
                 {
-                    if (bord.isBlauw(i, j)) // blauwe stenen
+                    if (bord.isPlayer1(i, j)) // blauwe stenen
                         g.DrawImage(Reversi.Properties.Resources.img_blue, steen[i, j]);
 
-                    else if (bord.isRood(i, j)) // rode stenen
+                    else if (bord.isPlayer2(i, j)) // rode stenen
                         g.DrawImage(Reversi.Properties.Resources.img_red, steen[i, j]);
 
+                    // Hulp rechthoeken tekenen als 'help' is aangevinkt.
                     if (bord.geldigeZet(i, j) && hulp)
                     {
                         g.DrawRectangle(Pens.Black, i * breedteVakje + b, j * hoogteVakje + b, breedteVakje - c, hoogteVakje - c);
@@ -122,17 +134,18 @@ namespace Reversi
             }
         }
 
+        // Nieuw spel button.
         private void button1_Click(object sender, EventArgs e)
         {
-            bord.NieuwSpel();
+            bord.nieuwSpel();
             panel1.Invalidate();
         }
 
+        // Help checkbox.
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             hulp = !hulp;
             panel1.Invalidate();
         }
-       
     }
 }
